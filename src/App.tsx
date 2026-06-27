8ee5613ad969f439b96085f34e6799676617ed30
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-26 22:42:50 -0500

    Agregado Register en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Register from './assets/pages/Register'
 import Navbar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Register/>
     </div>
   )
 }
