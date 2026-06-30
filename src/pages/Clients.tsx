2419a19b6c0332e6908662656ee3371c8740e0ef
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-06-30 18:58:00 -0500

    importado clientsTable a la página Clients.tsx

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,11 +1,13 @@
 import ClientsHeader from "../components/ClientsView/ClientsHeader";
 import ClientsListSection from "../components/ClientsView/ClientslistSection";
+import ClientsTable from "../components/ClientsView/ClientsTable";
 
 const Clients = () => {
   return (
     <div>
       <ClientsHeader/>
       <ClientsListSection/>
+      <ClientsTable/>
     </div>
   );
 };
