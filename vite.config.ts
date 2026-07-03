a1f0d529d96f24a85fdadc6b789b8e6cff8e6efd
AndresRios123 <andresc.rios@campusucc.edu.co>
2026-07-02 23:50:49 -0500

    actualizado server.proxy para localhost:3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // Agregamos server.proxy para que las peticiones /api/* del frontend se redirijan al backend en http://localhost:3000
+  server: {
+    proxy: {
+      "/api" : "http://localhost:3000",
+    }
+  }
 })
